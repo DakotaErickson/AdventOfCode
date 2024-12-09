e92732a1c5e8f9272ffa154a958d948ae68ada06
Dakota Erickson <dakotaerickson@Dakotas-M1.local>
2024-12-09 12:35:51 -0600

    map the strings to ints once to remove repeated .toInt()

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,26 +4,26 @@ fun main() {
     fun part1(input: List<String>): Int {
         var safeCount = 0
         for (line in input) {
-            val list = line.split(" ")
-            val isIncreasing = list[0].toInt() < list[1].toInt()
+            val list = line.split(" ").map { it.toInt() }
+            val isIncreasing = list[0] < list[1]
             var i = 0
 
             while (i < list.size - 1) {
 
                 // a line is unsafe if it changes from increasing to decreasing
-                if (isIncreasing && (list[i].toInt() > list[i+1].toInt())) {
+                if (isIncreasing && (list[i] > list[i+1])) {
                     break
                 }
                 // a line is unsafe if it changes from decreasing to increasing
-                if (!isIncreasing && list[i].toInt() < list[i+1].toInt()) {
+                if (!isIncreasing && list[i] < list[i+1]) {
                     break
                 }
                 // a line is unsafe if it moves more than 3 between adjacent numbers
-                if (abs(list[i].toInt() - list[i+1].toInt()) > 3) {
+                if (abs(list[i] - list[i+1]) > 3) {
                     break
                 }
                 // a line is unsafe if it doesn't change from one number to the next
-                if (list[i].toInt() == list[i+1].toInt()) {
+                if (list[i] == list[i+1]) {
                     break
                 }
 
@@ -42,36 +42,36 @@ fun main() {
         var safeCount = 0
 
         for (line in input) {
-            val list = line.split(" ")
-            val isIncreasing = list[0].toInt() < list[1].toInt()
+            val list = line.split(" ").map { it.toInt() }
+            val isIncreasing = list[0] < list[1]
             var i = 0
             var unsafeLevels = 0
 
             while (i < list.size - 1) {
 
                 // a line is unsafe if it changes from increasing to decreasing
-                if (isIncreasing && (list[i].toInt() > list[i+1].toInt())) {
+                if (isIncreasing && (list[i] > list[i+1])) {
                     unsafeLevels += 1
                     if (unsafeLevels > 1) {
                         break
                     }
                 }
                 // a line is unsafe if it changes from decreasing to increasing
-                if (!isIncreasing && list[i].toInt() < list[i+1].toInt()) {
+                if (!isIncreasing && list[i] < list[i+1]) {
                     unsafeLevels += 1
                     if (unsafeLevels > 1) {
                         break
                     }
                 }
                 // a line is unsafe if it moves more than 3 between adjacent numbers
-                if (abs(list[i].toInt() - list[i+1].toInt()) > 3) {
+                if (abs(list[i] - list[i+1]) > 3) {
                     unsafeLevels += 1
                     if (unsafeLevels > 1) {
                         break
                     }
                 }
                 // a line is unsafe if it doesn't change from one number to the next
-                if (list[i].toInt() == list[i+1].toInt()) {
+                if (list[i] == list[i+1]) {
                     unsafeLevels += 1
                     if (unsafeLevels > 1) {
                         break
@@ -91,6 +91,6 @@ fun main() {
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
-//    part1(input).println()
+    part1(input).println()
     part2(input).println()
 }
