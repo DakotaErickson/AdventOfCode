c8846a73251ea23f135cbf0cca8004de01bb05de
Dakota Erickson <dakotaerickson@Dakotas-M1.local>
2024-12-09 12:22:24 -0600

    day02 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -39,12 +39,58 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        println(input)
-        return 0
+        var safeCount = 0
+
+        for (line in input) {
+            val list = line.split(" ")
+            val isIncreasing = list[0].toInt() < list[1].toInt()
+            var i = 0
+            var unsafeLevels = 0
+
+            while (i < list.size - 1) {
+
+                // a line is unsafe if it changes from increasing to decreasing
+                if (isIncreasing && (list[i].toInt() > list[i+1].toInt())) {
+                    unsafeLevels += 1
+                    if (unsafeLevels > 1) {
+                        break
+                    }
+                }
+                // a line is unsafe if it changes from decreasing to increasing
+                if (!isIncreasing && list[i].toInt() < list[i+1].toInt()) {
+                    unsafeLevels += 1
+                    if (unsafeLevels > 1) {
+                        break
+                    }
+                }
+                // a line is unsafe if it moves more than 3 between adjacent numbers
+                if (abs(list[i].toInt() - list[i+1].toInt()) > 3) {
+                    unsafeLevels += 1
+                    if (unsafeLevels > 1) {
+                        break
+                    }
+                }
+                // a line is unsafe if it doesn't change from one number to the next
+                if (list[i].toInt() == list[i+1].toInt()) {
+                    unsafeLevels += 1
+                    if (unsafeLevels > 1) {
+                        break
+                    }
+                }
+
+                i += 1
+            }
+
+            // a line is safe if we've passed all the previous checks
+            if (i == list.size - 1) {
+                safeCount += 1
+            }
+        }
+        return safeCount
     }
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
-    part1(input).println()
-//    part2(input).println()
+//    part1(input).println()
+    part2(input).println()
 }
